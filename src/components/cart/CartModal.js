cb255a911bb031c0bb0fe4431fd147e0e3c28943
itsechi <wardulinskao@gmail.com>
2023-03-01 22:31:51 +0100

    Style the cart items

--- src/components/cart/CartModal.js
+++ src/components/cart/CartModal.js
@@ -3,41 +3,52 @@ import React from "react";
 
 function CartModal(props) {
   function decreaseQuantity(e) {
-    const id = e.target.parentElement.parentElement.dataset.id;
+    const id = e.target.closest('article').dataset.id;
     const product = props.cart.find((obj) => obj.item.id === id);
     if (product.quantity === 1) props.removeFromCart(product.item);
     else props.addToCart(product.item, product.quantity - 1);
   }
 
   function increaseQuantity(e) {
-    const id = e.target.parentElement.parentElement.dataset.id;
+    const id = e.target.closest('article').dataset.id;
     const product = props.cart.find((obj) => obj.item.id === id);
     props.addToCart(product.item, product.quantity + 1);
   }
 
   function handleChange(e) {
-    const id = e.target.parentElement.parentElement.dataset.id;
+    const id = e.target.closest('article').dataset.id;
     const product = props.cart.find((obj) => obj.item.id === id);
     props.addToCart(product.item, +e.target.value);
   }
 
   const cartItems = props.cart.map((obj) => (
-    <div key={obj.item.id} data-id={obj.item.id}>
-      <img src={obj.item.image} width="200" alt=""></img>
-      <p>{obj.item.name}</p>
-      <p>Category: {obj.item.category}</p>
-      <p>{obj.item.price}</p>
-      <div>
-        <button onClick={decreaseQuantity}>-</button>
-        <input
-          type="number"
-          value={obj.quantity}
-          onChange={handleChange}
-          min="0"
-        ></input>
-        <button onClick={increaseQuantity}>+</button>
+    <article className={styles.product} key={obj.item.id} data-id={obj.item.id}>
+      <img src={obj.item.image} alt="" className={styles.productImg}></img>
+      <div className={styles.productInfo}>
+        <div className={styles.productInfoTop}>
+          <p>{obj.item.name}</p>
+          <p className={styles.productDescription}>
+            Category: {obj.item.category}
+          </p>
+          <span className="material-icons-outlined">close</span>
+        </div>
+
+        <div className={styles.productInfoBottom}>
+          <div>
+            <button className={styles.quantityBtn} onClick={decreaseQuantity}>-</button>
+            <input
+              type="number"
+              value={obj.quantity}
+              onChange={handleChange}
+              className={styles.quantityInput}
+              min="0"
+            ></input>
+            <button className={styles.quantityBtn} onClick={increaseQuantity}>+</button>
+          </div>
+          <p>{obj.item.price}</p>
+        </div>
       </div>
-    </div>
+    </article>
   ));
 
   return (
@@ -50,7 +61,7 @@ function CartModal(props) {
             close
           </span>
         </div>
-        {cartItems}
+        <div className={styles.products}>{cartItems}</div>
         <div className={styles.buttons}>
           <button className={styles.btnSecondary} onClick={props.toggleModal}>
             Continue shopping
